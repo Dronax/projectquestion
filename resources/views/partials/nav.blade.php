0935563fbb97bfd98e570a71842b3e833aff05bb
Dronax <fantommd5@gmail.com>
2019-10-14 12:26:45 +0300

    fix

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -50,7 +50,7 @@
                         </a>
 
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a href="{{ route('profile', Auth::user()) }}" class="dropdown-item">Мой профиль</a>
+                            <a href="{{ route('profile', Auth::user()) }}" class="dropdown-item">My profile</a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
